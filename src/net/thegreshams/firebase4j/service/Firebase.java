e5275589300c8db0b7e34a9b4d9c57a331d28506
Brandon Gresham <brandon@thegreshams.net>
2012-05-05 12:46:48 -0600

    Added license info, log4j-properties, Firebase-constructor and main-runner (pulling base-url from program args).

--- src/net/thegreshams/firebase4j/service/Firebase.java
+++ src/net/thegreshams/firebase4j/service/Firebase.java
@@ -7,27 +7,65 @@ public class Firebase {
 	
 	protected static final Logger LOGGER = Logger.getRootLogger();
 	
-	
-	private Firebase() { /* DO NOTHING */ }
 
-	
 	
 ///////////////////////////////////////////////////////////////////////////////
 //
-//PUBLIC API
+// PROPERTIES & CONSTRUCTORS
 //
 ///////////////////////////////////////////////////////////////////////////////
-
 	
-	private static String baseUrl;
-	public static void setBaseUrl( String baseUrl ) throws FirebaseException {
-		
+	
+	private final String baseUrl;
+	
+	
+	public Firebase( String baseUrl ) throws FirebaseException {
+
 		if( baseUrl == null || baseUrl.trim().isEmpty() ) {
 			String msg = "baseUrl cannot be null or empty; was: '" + baseUrl + "'";
 			LOGGER.error( msg );
 			throw new FirebaseException( msg );
 		}
-		Firebase.baseUrl = baseUrl.trim();
+		this.baseUrl = baseUrl.trim();
+		LOGGER.info( "intialized with base-url: " + this.baseUrl );
+	}
+
+	
+	
+///////////////////////////////////////////////////////////////////////////////
+//
+// PUBLIC API
+//
+///////////////////////////////////////////////////////////////////////////////
+
+	
+	
+	
+	
+
+	
+///////////////////////////////////////////////////////////////////////////////
+//
+// MAIN
+//
+///////////////////////////////////////////////////////////////////////////////
+	
+	public static void main(String[] args) throws FirebaseException {
+
+		// get the base-url (ie: 'http://gamma.firebase.com/username')
+		String firebase_baseUrl = null;
+		for( String s : args ) {
+
+			if( s == null || s.trim().isEmpty() ) continue;
+			if( s.trim().split( "=" )[0].equals( "baseUrl" ) ) {
+				firebase_baseUrl = s.trim().split( "=" )[1];
+			}
+		}
+		if( firebase_baseUrl == null || firebase_baseUrl.trim().isEmpty() ) {
+			throw new IllegalArgumentException( "Program-argument 'baseUrl' not found but required" );
+		}
+	
+		Firebase firebase = new Firebase( firebase_baseUrl );
 	}
 	
 }
