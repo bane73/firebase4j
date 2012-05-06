feed53884d9fb32cf3f227bcf270284865708342
Brandon Gresham <brandon@thegreshams.net>
2012-05-05 20:25:54 -0600

    Added first draft of model for response, methods PUT and GET for service, and support for all of the above.

---
+++ src/net/thegreshams/firebase4j/model/FirebaseResponse.java
@@ -0,0 +1,77 @@
+package net.thegreshams.firebase4j.model;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+public class FirebaseResponse {
+	
+	protected static final Logger LOGGER = Logger.getRootLogger();
+	
+
+	
+///////////////////////////////////////////////////////////////////////////////
+//
+// PROPERTIES & CONSTRUCTORS
+//
+///////////////////////////////////////////////////////////////////////////////
+
+	private final boolean success;
+	private final int code;
+	private final Map<String, Object> body;
+	private final String rawBody;
+	
+	public FirebaseResponse( boolean success, int code, Map<String, Object> body, String rawBody ) {
+		
+		this.success = success;
+		this.code = code;
+		this.body = body;
+		this.rawBody = rawBody;
+	}
+	
+	
+	
+///////////////////////////////////////////////////////////////////////////////
+//
+// PUBLIC API
+//
+///////////////////////////////////////////////////////////////////////////////
+
+	
+	/**
+	 * Returns whether or not the response from the Firebase-client was successful
+	 * 
+	 * @return true if response from the Firebase-client was successful
+	 */
+	public boolean getSuccess() {
+		return this.success;
+	}
+	
+	/**
+	 * Returns the HTTP status code returned from the Firebase-client
+	 * 
+	 * @return an integer representing an HTTP status code
+	 */
+	public int getCode() {
+		return this.code;
+	}
+	
+	/**
+	 * Returns a map of the data returned by the Firebase-client
+	 * 
+	 * @return a map of Strings to Objects
+	 */
+	public Map<String, Object> getBody() {
+		return this.body;
+	}
+	
+	/**
+	 * Returns the raw data response returned by the Firebase-client
+	 * 
+	 * @return a String of the JSON-response from the client
+	 */
+	public String getRawBody() {
+		return this.rawBody;
+	}
+	
+}
--- src/net/thegreshams/firebase4j/service/Firebase.java
+++ src/net/thegreshams/firebase4j/service/Firebase.java
@@ -1,11 +1,36 @@
 package net.thegreshams.firebase4j.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 
 public class Firebase {
 	
-	protected static final Logger LOGGER = Logger.getRootLogger();
+	protected static final Logger 			LOGGER 					= Logger.getRootLogger();
+	
+	public static final String				FIREBASE_API_JSON_EXTENSION
+																	= ".json";
 	
 
 	
@@ -34,14 +59,298 @@ public class Firebase {
 	
 ///////////////////////////////////////////////////////////////////////////////
 //
+// STATIC PUBLIC API
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+	/**
+	 * Creates a json-string representing the data provided by the map.
+	 * 
+	 * @param dataMap; can be null/empty, but will result in a null result;
+	 * 			otherwise, must be Strings mapped to arbitrary Objects.
+	 * @return the json-string representing the data
+	 * @throws FirebaseException if there was an error converting the map-data into a json-string
+	 */
+	public static String GET_JSON_STRING_FROM_MAP( Map<String, Object> dataMap ) throws FirebaseException {
+	
+		/* NOTE: per Jackson-dox, the map must be of type <String, Object> */
+		
+		if( dataMap == null || dataMap.isEmpty() ) {
+			LOGGER.info( "cannot convert data from map into json when map is null/empty" );
+			return null;
+		}
+		
+		Writer writer = new StringWriter();		
+		try {
+		
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.writeValue( writer, dataMap );
+			
+		} catch( Throwable t ) {
+			
+			String msg = "unable to convert data from map into json: " + dataMap.toString();
+			LOGGER.warn( msg );
+			throw new FirebaseException( msg );
+			
+		}
+		
+		return writer.toString();
+	}
+
+	/**
+	 * Creates a map represented by the json-data provided.
+	 * 
+	 * @param jsonResponse; can be null/empty, but will result in a null result;
+	 * @return Strings mapped to arbitrary Objects
+	 * 
+	 * @throws FirebaseException if there was an error converting the json-string into map-data
+	 */
+	/* 'unchecked' because Jackson-dox state that a JSON-Object will always return as Map<String, Object>
+	 * http://wiki.fasterxml.com/JacksonDataBinding
+	 */	
+	@SuppressWarnings("unchecked")
+	public static Map<String, Object> GET_JSON_STRING_AS_MAP( String jsonResponse ) throws FirebaseException {
+		
+		if( jsonResponse == null || jsonResponse.trim().isEmpty() ) {
+			LOGGER.warn( "jsonResponse was null/empty, returning empty map; was: '" + jsonResponse + "'" );
+			return new HashMap<String, Object>();
+		}
+		jsonResponse = jsonResponse.trim();
+		
+		
+		Map<String, Object> result = null;		
+		try {
+			
+			ObjectMapper mapper = new ObjectMapper();
+			Object o = mapper.readValue( jsonResponse, Object.class );
+			if( o instanceof Map ) {
+				result = (Map<String, Object>) o;
+			}
+			
+		} catch( Throwable t ) {
+			
+			String msg = "unable to map json-response: " + jsonResponse; 
+			LOGGER.error( msg );
+			throw new FirebaseException( msg, t );
+			
+		}
+		
+		return result;
+	}
+
+	
+	
+///////////////////////////////////////////////////////////////////////////////
+//
 // PUBLIC API
+//
+///////////////////////////////////////////////////////////////////////////////
+	
+
+	/**
+	 * GETs data from the root-url.
+	 * 
+	 * @return data
+	 * @throws FirebaseException
+	 */
+	public Map<String, Object> get() throws FirebaseException {
+		return this.get( null );
+	}
+	
+	/** GETs data from the provided-path relative to the root-url.
+	 * 
+	 * @param path -- if null/empty, refers to root-url
+	 * @return data
+	 * @throws FirebaseException
+	 */
+	public Map<String, Object> get( String path ) throws FirebaseException {
+		
+		if( path != null && !path.startsWith( "/" ) ) {
+			path = "/" + path;
+		}
+		if( path == null ) {
+			path = "";
+		}
+		
+		String jsonResponse = this.getJsonData( this.baseUrl + path + FIREBASE_API_JSON_EXTENSION );
+		Map<String, Object> result = GET_JSON_STRING_AS_MAP( jsonResponse );
+		
+		return result;
+	}
+	
+	/** 
+	 * PUTs data to the root-url (ie: creates or overwrites).
+	 * If there is already data at the root-url, this data overwrites it.
+	 * If data is null/empty, any data existing at the root-url is deleted.
+	 *  
+	 * @param jsonData -- can be null/empty
+	 * @throws FirebaseException 
+	 */
+	public Map<String, Object> put( String jsonData ) throws FirebaseException {
+		return this.put( null, jsonData );
+	}
+	
+	/**
+	 * PUTs data to the provided-path relative to the root-url (ie: creates or overwrites)
+	 * If there is already data at the path, this data overwrites it.
+	 * If data is null/empty, any data existing at the path is deleted.
+	 * 
+	 * @param path -- if null/empty, refers to root-url
+	 * @param jsonData -- can be null/empty
+	 * @throws FirebaseException 
+	 */
+	public Map<String, Object> put( String path, String jsonData ) throws FirebaseException {
+		
+		if( path != null && !path.startsWith( "/" ) ) {
+			path = "/" + path;
+		}
+		if( path == null ) {
+			path = "";
+		}
+		
+		String jsonResponse = this.putJsonData( this.baseUrl + path + FIREBASE_API_JSON_EXTENSION, jsonData );
+		Map<String, Object> result = GET_JSON_STRING_AS_MAP( jsonResponse );
+		
+		return result;
+	}
+	
+	/**
+	 * POSTs data to the provided-path relative to the root-url (ie: modifies)
+	 * Note: the Firebase API calls this 'PUSH'; rather than standard POST-behavior, this method will 
+	 * 			insert the data into the provided-path but will be associated with a Firebase-assigned
+	 * 			id.  
+	 * 
+	 * @param path
+	 */
+	public void post( String path, String jsonData ) {
+		
+	}
+	
+
+	
+///////////////////////////////////////////////////////////////////////////////
+//
+// PRIVATE API
 //
 ///////////////////////////////////////////////////////////////////////////////
 
 	
+	private String putJsonData( String putUrl, String jsonData ) throws FirebaseException {
+		
+		if( putUrl == null || putUrl.trim().isEmpty() ) {
+			String msg = "putUrl cannot be null/empty; was: '" + putUrl + "'";
+			LOGGER.warn( msg );
+			throw new FirebaseException( msg );
+		}
+		putUrl = putUrl.trim();
+		
+		StringBuilder result = new StringBuilder();
+		InputStream is = null;
+		try {
+			
+			HttpClient client = new DefaultHttpClient();
+			HttpPut request = new HttpPut( putUrl );
+			StringEntity entity = new StringEntity( jsonData );
+			request.setEntity( entity );
+			HttpResponse response = client.execute( request );
+			HttpEntity responseEntity = response.getEntity();
+
+			Writer writer = new StringWriter();
+
+/*BJG*/ System.out.println( "\n\nSuccess? " + response.getStatusLine().getReasonPhrase() );
+/*BJG*/ System.out.println( "Code: " + response.getStatusLine().getStatusCode() + "\n\n" );
+			
+			if( responseEntity != null ) {
+				is = responseEntity.getContent();
+				char[] buffer = new char[1024];
+				Reader reader = new BufferedReader( new InputStreamReader( is, "UTF-8" ) );
+				int n;
+				while( (n=reader.read(buffer)) != -1 ) {
+					writer.write( buffer, 0, n );
+				}
+			}
+			result.append( writer.toString() );
+			
+			
+		} catch( Throwable t ) {
+			
+			
+		} finally {
+			
+			// apache http-components states we should always try and close the input-stream
+			if( is != null ) {
+				try {
+					is.close();
+				} catch( IOException e ) {
+					LOGGER.warn( "error closing input-stream", e );
+				}
+			}
+			
+		}
+		
+		LOGGER.debug( "response-data of PUT-request(" + putUrl + ") was: " + result.toString() );
+		
+		return result.toString();
+	}
 	
-	
-	
+	private String getJsonData( String getUrl ) throws FirebaseException {
+
+		if( getUrl == null || getUrl.trim().isEmpty() ) {
+			String msg = "getUrl cannot be null/empty; was: '" + getUrl + "'";
+			LOGGER.warn( msg );
+			throw new FirebaseException( msg );
+		}
+		getUrl = getUrl.trim();
+		
+		StringBuilder result = new StringBuilder();
+		InputStream is = null;
+		try {
+
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet( getUrl );			
+			HttpResponse response = client.execute( request );
+			HttpEntity entity = response.getEntity();
+
+			Writer writer = new StringWriter();
+
+/*BJG*/ System.out.println( "\n\nSuccess? " + response.getStatusLine().getReasonPhrase() );
+/*BJG*/ System.out.println( "Code: " + response.getStatusLine().getStatusCode() + "\n\n" );
+
+			if( entity != null ) {
+				is = entity.getContent();
+				char[] buffer = new char[1024];
+				Reader reader = new BufferedReader( new InputStreamReader( is, "UTF-8" ) );
+				int n;
+				while( (n=reader.read(buffer)) != -1 ) {
+					writer.write( buffer, 0, n );
+				}
+			}
+			result.append( writer.toString() );
+			
+		} catch( Throwable t ) {
+			
+			String msg = "unable to perform GET-request(" + getUrl + ")";
+			LOGGER.error( msg );
+			throw new FirebaseException( msg, t );
+			
+		} finally {
+			
+			// apache http-components states we should always try and close the input-stream
+			if( is != null ) {
+				try {
+					is.close();
+				} catch( IOException e ) {
+					LOGGER.warn( "error closing input-stream", e );
+				}
+			}
+			
+		}
+		
+		LOGGER.debug( "response-data of GET-request(" + getUrl + ") was: " + result.toString() );
+		
+		return result.toString();
+	}
 
 	
 ///////////////////////////////////////////////////////////////////////////////
@@ -50,8 +359,9 @@ public class Firebase {
 //
 ///////////////////////////////////////////////////////////////////////////////
 	
-	public static void main(String[] args) throws FirebaseException {
+	public static void main(String[] args) throws FirebaseException, JsonParseException, JsonMappingException, IOException {
 
+		
 		// get the base-url (ie: 'http://gamma.firebase.com/username')
 		String firebase_baseUrl = null;
 		for( String s : args ) {
@@ -64,8 +374,49 @@ public class Firebase {
 		if( firebase_baseUrl == null || firebase_baseUrl.trim().isEmpty() ) {
 			throw new IllegalArgumentException( "Program-argument 'baseUrl' not found but required" );
 		}
-	
+
+		// create the firebase
 		Firebase firebase = new Firebase( firebase_baseUrl );
+	
+		
+		// "GET" (the root)
+		Map<String, Object> map = firebase.get();
+		Iterator<String> it = map.keySet().iterator();
+		System.out.println( "\n\nResult of GET:" );
+		while( it.hasNext() ) {
+			String key = it.next();
+			System.out.println( key + "->" + map.get(key) );
+		}
+		System.out.println("\n");
+		
+		// "GET" (the food-coop)
+		map = firebase.get( "utah_food_coop" );
+		it = map.keySet().iterator();
+		System.out.println( "\n\nResult of GET (for the food-coop):" );
+		while( it.hasNext() ) {
+			String key = it.next();
+			System.out.println( key + "->" + map.get(key) );
+		}
+		System.out.println("\n");
+		
+		
+		// "PUT" (test-map into a sub-node off of the root)
+		Map<String, Object> dataMap = new LinkedHashMap<String, Object>();
+		dataMap.put( "Key_1", "This is the first value" );
+		dataMap.put( "Key_2", "This is value #2" );
+		Map<String, Object> dataMap2 = new LinkedHashMap<String, Object>();
+		dataMap2.put( "Sub-Key1", "This is the first sub-value" );
+		dataMap.put( "Key_3", dataMap2 );
+		String jsonData = GET_JSON_STRING_FROM_MAP( dataMap );
+		map = firebase.put( "test-PUT", jsonData );
+		it = map.keySet().iterator();
+		System.out.println( "\n\nResult of PUT (for the test-PUT):" );
+		while( it.hasNext() ) {
+			String key = it.next();
+			System.out.println( key + "->" + map.get(key) );
+		}
+		System.out.println("\n");
+		
 	}
 	
 }
